2fdc1deb6fe8adf8cdaa1cec9c0d6512baa4da28
boobooL <boobooMX@163.com>
2016-04-27 10:42:43 +0800

    需要的类库添加完毕

--- app/src/main/java/com/mx/MainActivity.java
+++ app/src/main/java/com/mx/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mx;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialSpinner spinnerFirstOnly;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
