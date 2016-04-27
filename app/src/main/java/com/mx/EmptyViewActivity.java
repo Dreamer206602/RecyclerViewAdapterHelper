63e9ab92b728824c78577a92743991deee662bc2
boobooL <boobooMX@163.com>
2016-04-27 13:49:10 +0800

    Adapter可以添加空的View

--- app/src/main/java/com/mx/EmptyViewActivity.java
+++ app/src/main/java/com/mx/EmptyViewActivity.java
@@ -1,13 +1,36 @@
 package com.mx;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mx.adapter.QuickAdapter;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class EmptyViewActivity extends AppCompatActivity {
+
+    @InjectView(R.id.rv_list)
+    RecyclerView mRvList;
+    private QuickAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_view);
+        ButterKnife.inject(this);
+
+        mRvList.setHasFixedSize(true);
+
+
+        mAdapter = new QuickAdapter(this, 0);
+        View emptyView = getLayoutInflater().inflate(R.layout.empty_view, (ViewGroup) mRvList.getParent(), false);
+        mAdapter.setEmptyView(emptyView);
+        mRvList.setAdapter(mAdapter);
+
+
     }
 }
